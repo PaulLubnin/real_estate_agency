0e18bc5d6da443bc82584ffcfe65e15756828fed
paulego <egopaul@inbox.ru>
2023-04-17 09:45:01 +0500

    Normalize Flat.phonenumbers into Flat.owner_pure_phone.

--- property/migrations/0004_auto_20230404_1246.py
+++ property/migrations/0004_auto_20230404_1246.py
@@ -9,12 +9,16 @@ def fill_new_building_field(apps, schema_editor):
     Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
 
 
-class Migration(migrations.Migration):
+def move_backward(apps, schema_editor):
+    Flat = apps.get_model('property', 'Flat')
+    Flat.objects.update(new_building=None)
 
+
+class Migration(migrations.Migration):
     dependencies = [
         ('property', '0003_flat_new_building'),
     ]
 
     operations = [
-        migrations.RunPython(fill_new_building_field)
+        migrations.RunPython(fill_new_building_field, move_backward)
     ]
